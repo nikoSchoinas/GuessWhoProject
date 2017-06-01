0a9f8aa74781951b6786d67d6eb8faf15828fb22
kostas bousinis <dai16108@uom.edu.gr>
2017-06-01 19:47:53 +0300

    add WarningCreateAccount class

---
+++ src/game/WarningCreateAccount.java
@@ -0,0 +1,53 @@
+package game;
+
+import javax.swing.JFrame;
+import java.awt.Toolkit;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+
+public class WarningCreateAccount {
+
+	public JFrame frmGuessWho;
+	private String output;
+
+
+	/**
+	 * Create the application.
+	 */
+	public WarningCreateAccount(String output) {
+		this.output = output;
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmGuessWho = new JFrame();
+		frmGuessWho.setTitle("Guess Who?");
+		frmGuessWho.setIconImage(Toolkit.getDefaultToolkit().getImage("C:\\Users\\kostas\\Desktop\\Game\\533038-guess-who-windows-screenshot-the-game-s-title-screen.png"));
+		frmGuessWho.setBounds(100, 100, 553, 207);
+		frmGuessWho.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmGuessWho.getContentPane().setLayout(null);
+		frmGuessWho.setLocationRelativeTo(null);
+		
+		JButton OkButton = new JButton("OK");
+		OkButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frmGuessWho.dispose();
+			}
+		});
+		OkButton.setBounds(208, 96, 123, 37);
+		frmGuessWho.getContentPane().add(OkButton);
+		
+		JLabel WarningMessage = new JLabel(output);
+		WarningMessage.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		WarningMessage.setHorizontalAlignment(SwingConstants.CENTER);
+		WarningMessage.setBounds(22, 41, 501, 42);
+		frmGuessWho.getContentPane().add(WarningMessage);
+	}
+}
